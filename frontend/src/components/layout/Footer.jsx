b23fed696ce7c8a6078cb3da047b915a57563c3a
Vivek sharma <vivek.sharma1275@gmail.com>
2025-08-21 15:31:25 +0530

    changes

--- frontend/src/components/layout/Footer.jsx
+++ frontend/src/components/layout/Footer.jsx
@@ -4,7 +4,12 @@ const Footer = () => {
       <div className="flex items-center justify-center px-4">
         <h1 className="text-lg md:text-xl font-semibold text-center">
           &copy; {new Date().getFullYear()} All Rights Reserved. Developed by{" "}
-          <span className="text-yellow-400 font-bold">Vivek Sharma</span>
+          <span
+            className="bg-gradient-to-r from-yellow-400 via-orange-500 to-red-500 
+            bg-clip-text text-transparent font-bold"
+          >
+            Vivek Sharma
+          </span>
         </h1>
       </div>
     </footer>
