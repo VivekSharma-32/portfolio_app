c963c0151f834ca9aef82daad44cf115b0a9312a
Vivek sharma <vivek.sharma1275@gmail.com>
2025-08-29 09:58:36 +0530

    changes

--- backend/src/config/config.js
+++ backend/src/config/config.js
@@ -1,11 +1,13 @@
 const mongoose = require("mongoose");
 
 const connectDb = async () => {
-  await mongoose.connect("mongodb://localhost:27017/portfolio_db", {
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
-  });
-  console.log(`Connected with the database successfully`);
+  try {
+    await mongoose.connect(process.env.DATABASE_URL);
+    console.log(`Connected with the database successfully`);
+  } catch (error) {
+    console.error("Error connecting with the database:");
+    console.error(error);
+  }
 };
 
 module.exports = connectDb;
